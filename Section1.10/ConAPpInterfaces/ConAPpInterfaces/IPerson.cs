6ea68042ffe873d6956d85af620de998d45a6929
PrashastVats1 <prashastvats@gmail.com>
2023-08-08 16:41:34 +0530

    Assisted Projects

---
+++ Section1.10/ConAPpInterfaces/ConAPpInterfaces/IPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAPpInterfaces
+{
+    public interface IPerson
+    {
+        string getInfo();
+        string getTypeOfPerson();
+    }
+
+}
